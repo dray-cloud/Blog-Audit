f616278abe974ac31461c003deb0ed1c5a4590a6
dray-cloud <devynaray@yahoo.com>
2026-03-19 13:51:11 -0400

    Add Vercel Speed Insights

--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,11 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { inject } from "@vercel/analytics";
+import { injectSpeedInsights } from "@vercel/speed-insights";
 import App from "../blog-auditor.jsx";
 
 inject();
+injectSpeedInsights();
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
